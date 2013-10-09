3354f02e2411a9af124f44280490351082d0ade2
Michael Wenger <michaelwenger27@gmail.com>
2013-10-08 20:33:05 -0400

    Added seed data

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131008234940) do
+ActiveRecord::Schema.define(version: 20131008235557) do
 
   create_table "shirts", force: true do |t|
     t.string   "name"
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
 end
